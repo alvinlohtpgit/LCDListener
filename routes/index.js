0abdba15d31ee53b2307eb0f20a46c252b66ee70
Alvin Loh <alvinloh@tuaspower.com.sg>
2019-02-26 15:40:49 +0800

    Server 1.0

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var center = require('center-align');
+
+var LCD = require('lcdi2c');
+var lcd = new LCD(1, 0x27, 16 , 2);
+
 
 
 /* GET home page. */
@@ -14,16 +19,22 @@ router.get('/', function(req, res, next) {
 
 router.post('/flash', function(req, res, next){
 
+  const lcdwidth = 16;
+
   var line1 = req.body.line1;
   var line2 = req.body.line2;
-
-
+  
+  lcd.clear();
 
   if (line1){
+    line1 = center(line1, lcdwidth);
+    lcd.println(line1, 1);
     console.log("Line 1 : " + line1);
   }
 
   if (line2){
+    line2 = center(line2, lcdwidth);
+    lcd.println(line2, 2);
     console.log("Line 2 : " + line2);
   }
 
