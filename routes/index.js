340454b3bc784b06c3e98fa104eea21e4904d58a
Alvin Loh <alvinloh@alvinloh.com>
2019-02-26 16:52:52 +0800

    Added code to parse POST

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
+// Method expects a JSON object to be send to it
+// E.g {"line1": "Line 1", "line2": "Line 2"}
+// then we just print the line onto the specified line on the LCD
+
+router.post('/flash', function(req, res, next){
+
+  var line1 = req.body.line1;
+  var line2 = req.body.line2;
+
+
+
+  if (line1){
+    console.log("Line 1 : " + line1);
+  }
+
+  if (line2){
+    console.log("Line 2 : " + line2);
+  }
+
+  res.send("Done");
+
+
+});
+
+
 
 module.exports = router;
